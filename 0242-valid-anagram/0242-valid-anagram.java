f1c0dd872d636b5cc063717021981f8248dfdf20
SaiIyer23 <saiiyer69@gmail.com>
2026-09-12 19:33:42 +0530

    Time: 14 ms (26.5%), Space: 46.7 MB (9.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        //int[] count=new int[26];
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)-1);
+        }
+        for(int freq:map.values()){
+            if(freq!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
